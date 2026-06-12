5311e7d6ab0017ce3531c040344b7425d65f7170
Marek Vondra <maravondra>
2026-06-12 12:27:33 +0200

    make the designt for thi

--- src/app/pages/calculator/calculator-page.component.ts
+++ src/app/pages/calculator/calculator-page.component.ts
@@ -1,15 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { StatisticCardComponent } from '../statistic-card/statistic-card.component';
 
 @Component({
   selector: 'app-calculator-page',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, StatisticCardComponent],
   templateUrl: './calculator-page.component.html',
   styleUrl: './calculator-page.component.scss', // <-- Zde: styleUrl místo styleUrls a bez hranatých závorek []
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalculatorPageComponent {
+export class CalculatorPageComponent implements OnInit {
+  isLoading = signal(false);
+  readonly defaultSalary = signal<number>(history.state?.salary ?? 0);
 
+  ngOnInit(): void {
+    this.isLoading.set(true);
 
+    // Simulace asynchronního načítání dat (např. z API) po dobu 5 sekund
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 2500); // 5000 milisekund = 5 sekund
+  }
 }
---
+++ src/app/pages/statistic-card/statistic-card.component.ts
@@ -0,0 +1,22 @@
+import {Component, input} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+export interface StatCardConfig {
+  label: string;
+  value: string | number;
+  icon: string;
+  cssClass?: string; // Pro speciální barvy jako 'gold'
+  suffix?: string;   // Např. 'Kč'
+}
+
+@Component({
+  selector: 'app-statistic-card',
+  imports: [MatIconModule],
+  templateUrl: './statistic-card.component.html',
+  styleUrl: './statistic-card.component.scss',
+  standalone: true,
+})
+export class StatisticCardComponent {
+  config = input.required<StatCardConfig>();
+  isLoading = input<boolean>(false);
+}
