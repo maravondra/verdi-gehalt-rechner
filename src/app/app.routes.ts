05aced3b6e79526b099f3661527ae93a81fbbf19
Marek Vondra <maravondra>
2026-07-02 09:59:51 +0200

    create impressum and add header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: '',
-		loadComponent: () => import('./pages/home/home-page.component').then((m) => m.HomePageComponent),
-	},
-	{
-		path: 'rechner',
-		loadComponent: () => import('./pages/calculator/calculator-page.component').then((m) => m.CalculatorPageComponent),
-	},
-	{
-		path: '**',
-		redirectTo: '',
-	},
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home-page.component').then((m) => m.HomePageComponent),
+  },
+  {
+    path: 'rechner',
+    loadComponent: () =>
+      import('./pages/calculator/calculator-page.component').then((m) => m.CalculatorPageComponent),
+  },
+  {
+    path: 'impressum',
+    loadComponent: () => import('./pages/impressum/impressum').then((m) => m.Impressum),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
